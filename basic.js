a7e7ba9a357400cc25cb08368a6205c3da571b13
Ranjeet Kumar <108877679+ranjeet-kumar-2002@users.noreply.github.com>
2024-06-19 21:06:55 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -24,3 +24,59 @@ if (Number(age) < 18) {
     console.log("You will not get 20% discount");
 }
 
+
+// method 1 for creating function
+function add(a,b){
+    return a+b;
+}
+
+// method 2 for creating function
+var add = function(a,b){
+    return a+b;
+}
+
+// // method 3 for creating function
+var add = (a,b)=>{
+    return a+b;
+}
+
+// method 4 for creating function
+var add = (a,b)=> a+b;
+
+// method 5 for creating function
+(function(){
+    console.log("ranjeet");
+})();
+
+var result = add(3,7);
+console.log(result);
+
+//******function calling another fucntion is call callback******callback fuction method1***********
+
+function callback(){
+    console.log("callback fucntion");
+}
+
+const add = function(a,b,callback){
+    var result = a+b;
+    console.log("result:"+result); // main function
+    callback(); // callback fucntion
+}
+
+add(3,4,callback);
+
+
+//******function calling another fucntion is call callback******callback fuction method2***********
+
+
+const add = function(a,b,callback){
+    var result = a+b;
+    console.log("result:"+result); // main function
+    callback(); // callback fucntion
+}
+add(2,3,function(){
+    console.log("add completed")
+});
+
+add(2,3,()=>console.log("add completed"));
+
